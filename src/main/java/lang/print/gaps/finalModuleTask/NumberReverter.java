6b54a669aff3618716184c04ede39e747b8e5b68
kirillnev <kirillnev84@gmail.com>
2023-11-17 09:15:56 +0600

    done

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int units = number % 10;
+        int tens = number % 100 / 10;
+        int hundreds = number / 100;
+        System.out.println(units * 100 + tens * 10 + hundreds);
     }
 }
